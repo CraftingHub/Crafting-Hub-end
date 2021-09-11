76c335947de0ce2d05b84a7e434a4dadd4fe6465
na0495 <saadmrabet008@gmail.com>
2021-09-11 22:57:33 +0100

    config new packages & create requiprement.txt

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'model_bakery',
+    'drf_yasg',
     'post',
 ]
 
